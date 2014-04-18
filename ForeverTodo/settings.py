ee49204ff17d67f00fcdd699fe6267bcae6f0a31
Sergey Orshanskiy <orshansk@gmail.com>
2014-04-18 19:07:54 -0400

    Switched database to Heroku's postgresql. I really need to separate local vs remote settings...

--- ForeverTodo/settings.py
+++ ForeverTodo/settings.py
@@ -125,8 +125,8 @@ THEME_ACCOUNT_CONTACT_EMAIL = "no-contact@sad.com"
 ### for Heroku deployment ###
 
 # Parse database configuration from $DATABASE_URL
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
