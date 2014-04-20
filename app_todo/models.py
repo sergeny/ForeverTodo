7f115815d37d1b302c8ebb7426b3f91f2356c0ce
Sergey Orshanskiy <orshansk@gmail.com>
2014-04-20 03:53:17 -0400

    Added to TodoItem  priority = models.IntegerField() ( 0 is low and 2 is high) and expires = models.DateTimeField(null=True, blank=True)

--- app_todo/models.py
+++ app_todo/models.py
@@ -5,4 +5,6 @@ class TodoItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=1000)
+    priority = models.IntegerField() # 0 is low and 2 is high
+    expires = models.DateTimeField(null=True, blank=True)
     completed = models.BooleanField()
